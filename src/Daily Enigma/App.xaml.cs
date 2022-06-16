d64f05433946b0cdc48f704fbee64312e24fbeb7
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-06-16 19:19:11 +0200

    [WIP] Create Interface

---
+++ src/Daily Enigma/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Daily_Enigma.Services;
+using Daily_Enigma.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Daily_Enigma
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/Daily Enigma/Views/OptionsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Daily_Enigma.Models;
+using Daily_Enigma.ViewModels;
+using Daily_Enigma.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Daily_Enigma.Views
+{
+    public partial class OptionsPage : ContentPage
+    {
+        OptionsViewModel _viewModel;
+
+        public OptionsPage()
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = new OptionsViewModel();
+        }
+    }
+}
