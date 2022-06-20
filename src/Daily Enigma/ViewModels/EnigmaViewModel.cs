bfd3989b582a1db1bf1267c26aa0687ead32cafa
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-06-20 14:40:27 +0200

    Revert "Reset part 1"
    This reverts commit 33c6b21a7db3bfba8badc46bcf2f0d79b08d90e5.

---
+++ src/Daily Enigma/ViewModels/EnigmaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Daily_Enigma.Models;
+using Daily_Enigma.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Daily_Enigma.ViewModels
+{
+    public class EnigmaViewModel : BaseViewModel
+    {
+        public EnigmaViewModel()
+        {
+            Title = "Daily Enigma";
+        }
+    }
+}
