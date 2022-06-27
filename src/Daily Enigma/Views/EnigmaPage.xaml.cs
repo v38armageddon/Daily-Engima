e0435bf3ea10080c1ee68a66b150f387758b46ca
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-06-27 11:12:04 +0200

    Begin Programming

--- src/Daily Enigma/Views/EnigmaPage.xaml.cs
+++ src/Daily Enigma/Views/EnigmaPage.xaml.cs
@@ -23,27 +23,29 @@ namespace Daily_Enigma.Views
             BindingContext = _viewModel = new EnigmaViewModel();
         }
 
-        public void RandomEnigma()
+        private void RandomEnigma(object sender, EventArgs e)
         {
             var enigmas = new List<string>()
             {
                 "My first is the main color of the earth, my second can be used to move, my third is for surviving. My everything is a liquid. What is that ?",
                 "Very childish, I can introduce children to programming. What is my name ?",
             };
+            int range = enigmas.Count;
+
+            Random rnd = new Random();
+            int number = rnd.Next(0, range - 1);  // creates a number between 0 and range-1
+
+            Label_Enigma.Text = enigmas[number];
         }
 
-        public void Timer()
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            var timer = new Timer();
-            timer.Interval = 1000;
-            timer.Elapsed += (sender, e) =>
+            string checkAnswer = Label_Answer.Text;
+
+            if (checkAnswer == "Foo")
             {
-                Device.BeginInvokeOnMainThread(() =>
-                {
-                    Label_Time.Text = DateTime.Now.ToString("HH:mm:ss");
-                });
-            };
-            timer.Start();
+                Label_Enigma.Text = "Bar";
+            }
         }
     }
 }
