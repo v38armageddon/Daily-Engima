65c14c175eda9a216f877f80d9d164fec5d2d32e
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-07-14 11:03:02 +0200

    Working on app I don't remember what I do changes

--- src/Daily Enigma/Views/EnigmaPage.xaml.cs
+++ src/Daily Enigma/Views/EnigmaPage.xaml.cs
@@ -22,18 +22,23 @@ namespace Daily_Enigma.Views
             InitializeComponent();
             BindingContext = _viewModel = new EnigmaViewModel();
         }
-
-        private void RandomEnigma(object sender, EventArgs e)
+        
+        public void RandomEnigma(object sender, EventArgs e)
         {
             var enigmas = new List<string>()
             {
                 "My first is the main color of the earth, my second can be used to move, my third is for surviving. My everything is a liquid. What is that ?",
                 "Very childish, I can introduce children to programming. What is my name ?",
+                "I'm famous by it's revolutionary product and I am also a fruit, what am I?",
+                "",
+                "",
+                "",
+                "",
             };
             int range = enigmas.Count;
 
             Random rnd = new Random();
-            int number = rnd.Next(0, range - 1);  // creates a number between 0 and range-1
+            int number = rnd.Next(0, range - 7);  // creates a number between 0 and range-1
 
             Label_Enigma.Text = enigmas[number];
         }
@@ -42,13 +47,49 @@ namespace Daily_Enigma.Views
         {
             string checkAnswer = Label_Answer.Text;
 
-            if (checkAnswer == "Foo")
+            if (checkAnswer == "Water")
             {
-                Label_Enigma.Text = "Bar";
+                if (Label_Enigma.Text == "My first is the main color of the earth, my second can be used to move, my third is for surviving. My everything is a liquid. What is that ?")
+                {
+                    DisplayAlert("Congratulation!", "You solved the enigma of the day!\n\nCome back tomorrow!", "OK");
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else if (checkAnswer == "Scratch")
+            {
+                if (Label_Enigma.Text == "Very childish, I can introduce children to programming. What is my name ?")
+                {
+                    DisplayAlert("Congratulation!", "You solved the enigma of the day!\n\nCome back tomorrow!", "OK");
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else if (checkAnswer == "Apple")
+            {
+                if (Label_Enigma.Text == "I'm famous by it's revolutionary product and I am also a fruit, what am I?")
+                {
+                    DisplayAlert("Congratulation!", "You solved the enigma of the day!\n\nCome back tomorrow!", "OK");
+                }
+                else
+                {
+                    return;
+                }
             }
             else
             {
-                
+                if (Label_Answer.Text == "")
+                {
+                    DisplayAlert("Error", "Please enter an answer", "OK");
+                }
+                else
+                {
+                    DisplayAlert("Try Again!", "This is not the correct answer...\n\nTry again or wait tomorrow.", "OK");
+                }
             }
         }
     }
